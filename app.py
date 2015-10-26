3c05e22aea5493c0a90fc070781ac5cfa9c8e254
luisan00 <luisan00@hotmail.com>
2015-10-26 15:08:39 +0100

    added new funtions.
    Added new function get address info.
    Added function to get bitcoind info.
    functions now return especific error messages

--- app.py
+++ app.py
@@ -12,12 +12,28 @@ import json
 from config import host, port
 
 # routes
-route = {'sync' : '/api/sync',
+route = {
+         'status': {
+                    'getInfo': '/api/status?q=getInfo',
+                    'getDifficulty': '/api/status?q=getDifficulty',
+                    'getBestBlockHash': '/api/status?q=getBestBlockHash',
+                    'getLastBlockHash': '/api/status?q=getLastBlockHash'
+                    },
+         'sync' : '/api/sync',
          'peer' : '/api/peer',
          'tx' : '/api/tx/%s',
-         'block' : '/api/block/%s'
+         'block' : '/api/block/%s',
+         'address': '/api/addr/%s'
          }
 
+# messages
+
+msg = {
+            '-1': 'error, connecting to server: %s:%s',
+            '-2' : 'error, transsaction: <%s> not found',
+            '-3' : 'error, block: <%s> not found',
+            '-4' : 'error, address: <%s> not found'
+            }
 
 class insight(object):
 			
@@ -26,21 +42,31 @@ class insight(object):
 		self.port = __port
 		self.connection = httplib.HTTPConnection(self.host, self.port)
 		
-	# Return sync. status 	
+	# Return various status messages.
+	
+	def status(self, args):
+		try:
+			self.connection.request('GET', route['status'][args])
+		except:
+			return (msg['-1'] % (host, port)
+		response = self.connection.getresponse()
+		return response.read()
+		
+	# Return sync. info.	
 	def sync(self):
 		try:
 			self.connection.request('GET', route['sync'])
 		except:
-			return ('error[-1] connecting to server')
+			return (msg['-1'] % (host, port)
 		response = self.connection.getresponse()
 		return response.read()
 		
-	# Return peer estatus	
+	# Return peer info.
 	def peer(self):
 		try:
 			self.connection.request('GET', route['peer'])
 		except:
-			return ('error[-1] connecting to server')
+			return (msg['-1'] % (host, port)
 		response = self.connection.getresponse()
 		return response.read()
 		
@@ -49,37 +75,61 @@ class insight(object):
 		try:
 			self.connection.request('GET', route['tx'] % txid)
 		except:
-			return ('error[-1] connecting to server')
+			return (msg['-1'] % (host, port)
 		response = self.connection.getresponse()
 		try: 
 			data = response.read()
 			this = json.loads(data, object_pairs_hook=OrderedDict)
 			return json.dumps(this, indent=4)
 		except:
-			return ('error[0] tx: <%s> not found'% txid)
+			return (msg['-2'] % txid)
 			
-	# Return a block by hash if exist		
+	# Return a block by hash if exist.	
 	def block(self,block):
 		try:
 			self.connection.request('GET', route['block'] % block)
 		except:
-			return ('error[-1] connecting to server')
+			return (msg['-1'] % (host, port)
 		response = self.connection.getresponse()
 		try: 
 			data = response.read()
 			this = json.loads(data, object_pairs_hook=OrderedDict)
 			return json.dumps(this, indent=4)
 		except:
-			return ('error[0] block: <%s> not found'% block)
+			return (msg['-3'] % block)
+			
+	# Return address properties if exist.
+	def address(self, addr):
+		try:
+			self.connection.request('GET', route['address'] % addr)
+		except:
+			return (msg['-1'] % (host, port)
+		response = self.connection.getresponse()
+		try: 
+			data = response.read()
+			this = json.loads(data, object_pairs_hook=OrderedDict)
+			return json.dump(this, indent=4)
+		except:
+			return (msg['-4'] % addr)
 			
 # Examples.
+#
 # first create the object in the variable explorer.
 # >> explorer = insight(host, port)
-# later can call the function peer, sync, getBlock(byHash), getTx(byTxId)
+#
+# you can call the function status, peer, sync, block(byHash), tx(byTxId), address(address)
+#
+# function status require a param: 'getInfo', 'getDifficulty', 'getBestBlockHash' or getLastBlockHash
+# >> bestBlockHash = explorer.status('getBestBlockHash')
+# 
+# peer, or sync no need options
 # >> peer = explorer.peer()
 # or
 # >> sync = explorer.sync()
+#
+# 
+# >> block = explorer.block('hash_of_the_block')
 # or
-# >> block = block('hash_of_the_block')
+# >> tx = explorer.tx('tx_id')
 # or
-# tx = explorer.tx('tx_id')              
+# >> address = explorer.address('address_to_search')          
